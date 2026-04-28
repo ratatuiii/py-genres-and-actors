f7f6e3f88895e3f9761e0f91ba5f6773544acdde
naomi <orest.chupa@gmail.com>
2026-04-28 11:31:28 +0300

    Fix flake8 issues

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from django.db.models import QuerySet
 
 from db.models import Actor, Genre
 
+
 def main() -> QuerySet:
-    western = Genre.objects.create(name="Western")
+    Genre.objects.create(name="Western")
     action = Genre.objects.create(name="Action")
     dramma = Genre.objects.create(name="Dramma")
 
     george = Actor.objects.create(first_name="George", last_name="Klooney")
     kianu = Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    scarlett = Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    will = Actor.objects.create(first_name="Will", last_name="Smith")
-    jaden = Actor.objects.create(first_name="Jaden", last_name="Smith")
-    scarlett_johansson = Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
     dramma.name = "Drama"
     dramma.save()
@@ -32,9 +33,3 @@ def main() -> QuerySet:
     Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
-
-
-
-
-
-
